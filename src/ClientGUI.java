8b57d8bf343e2ac79103e7527b1362a113638bdb
Amel <amelaljo@hotmail.de>
2016-09-01 14:35:08 +0200

    Merge remote-tracking branch 'origin/Eugen' into Amel
    # Conflicts:
    #	.idea/vcs.xml
    #	src/ClientGUI.form
    #	src/ClientGUI.java

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -227,49 +227,6 @@ public class ClientGUI extends JFrame implements ActionListener, TreeModel, Seri
         clientTextArea.setCaretPosition(clientTextArea.getText().length() - 1);
     }
 
-    /** Add nodes from under "dir" into curTop. Highly recursive. */
-
-    /*
-    DefaultMutableTreeNode addNodes(DefaultMutableTreeNode curTop, File dir)
-    {
-        String curPath = dir.getPath();
-        DefaultMutableTreeNode curDir = new DefaultMutableTreeNode(curPath);
-        if (curTop != null)
-        { // should only be null at root
-            curTop.add(curDir);
-        }
-        Vector ol = new Vector();
-        String[] tmp = dir.list();
-        for (int i = 0; i < tmp.length; i++)
-            ol.addElement(tmp[i]);
-        Collections.sort(ol, String.CASE_INSENSITIVE_ORDER);
-        File f;
-        Vector files = new Vector();
-        // Make two passes, one for Dirs and one for Files. This is #1.
-        for (int i = 0; i < ol.size(); i++)
-        {
-            String thisObject = (String) ol.elementAt(i);
-            String newPath;
-            if (curPath.equals(".")) {
-                newPath = thisObject;
-            }
-            else
-                newPath = curPath + File.separator + thisObject;
-            if ((f = new File(newPath)).isDirectory())
-            {
-                addNodes(curDir, f);
-            }
-            else
-                files.addElement(thisObject);
-        }
-        // Pass two: for files.
-        for (int fnum = 0; fnum < files.size(); fnum++)
-            curDir.add(new DefaultMutableTreeNode(files.elementAt(fnum)));
-        return curDir;
-    }
-
-*/
-
     /**
      * Button gedrückt
      */
@@ -451,18 +408,8 @@ public class ClientGUI extends JFrame implements ActionListener, TreeModel, Seri
             {
                 System.out.println("Fehler: " + eBrowse.getMessage());
             }
-            */
-            /*
-            // Make a tree list with all the nodes, and make it a JTree
-            //JTree tree = new JTree(addNodes(null, new File(pfad) ));
-            JTree tree = new JTree(addNodes(null, new File("\\") ));
 
-            // Lastly, put the JTree into a JScrollPane.
-            JScrollPane scrollpane = new JScrollPane();
-            scrollpane.getViewport().add(tree);
-            add(BorderLayout.CENTER, scrollpane);
 
-/
             //Where instance variables are declared:
             */
             File root = new File("\\");
@@ -489,8 +436,6 @@ public class ClientGUI extends JFrame implements ActionListener, TreeModel, Seri
 
             f.pack();
             f.setVisible(true);
-            pack();
-            setVisible(true);
         }
 
         if(o == searchButton)
@@ -530,10 +475,6 @@ public class ClientGUI extends JFrame implements ActionListener, TreeModel, Seri
                 searchFeld.setText("");
                 ersteEingabe = true;
             }
-
-
-
-
             //JFrame eingabe = new JFrame();
             //String pfad = JOptionPane.showInputDialog(eingabe, "Was soll gesucht werden?", "Seach", JOptionPane.PLAIN_MESSAGE);
             //String startDir = JOptionPane.showInputDialog(eingabe, "Wo soll gesucht werden?", "Seach", JOptionPane.PLAIN_MESSAGE);
